d5cb8f4503ef21a727b14e8c9426af0be08ef2e9
bsicay <sicayb@gmail.com>
2021-10-19 17:12:02 -0600

    archivos de ejemplo

---
+++ src/org/proyectofinal/controller/EjemploController.java
@@ -0,0 +1,31 @@
+package org.proyectofinal.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import org.proyectofinal.sistema.Principal;
+
+
+
+public class EjemploController implements Initializable{
+	private Principal escenarioPrincipal;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public Principal getEscenarioPrincipal() {
+        
+        return escenarioPrincipal;
+    }
+
+    public void setEscenarioPrincipal(Principal escenarioPrincipal) {
+        this.escenarioPrincipal = escenarioPrincipal;
+    }
+
+
+}
--- src/org/proyectofinal/sistema/Principal.java
+++ src/org/proyectofinal/sistema/Principal.java
@@ -1,10 +1,68 @@
 package org.proyectofinal.sistema;
 
-public class Principal {
+import java.io.InputStream;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+import org.proyectofinal.controller.EjemploController;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Principal extends Application {
+    private final String PAQUETE_VISTA = "/org/proyectofinal/view/";
+    private Stage escenarioPrincipal;
+    private Scene escena; 
+  
+    @Override
+    public void start(Stage escenarioPrincipal) {
+        this.escenarioPrincipal = escenarioPrincipal;
+        escenarioPrincipal.setTitle("RAM");
+       // escenarioPrincipal.getIcons().add(new Image("/org/brandonsicay/images/ram.png"));
+        iniciar();
+        escenarioPrincipal.show();
+        
+       
+    }
+    
+    public void iniciar(){
+        try{
+            EjemploController iniciar = (EjemploController)cambiarEscena("ejemploView.fxml", 600, 400);
+            iniciar.setEscenarioPrincipal(this);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    
+    }
+    
+    
+    
+    public Initializable cambiarEscena(String fxml, int ancho, int alto) throws Exception{
+        Initializable resultado = null; 
+        FXMLLoader cargadorFXML = new FXMLLoader();
+        InputStream archivo = Principal.class.getResourceAsStream(PAQUETE_VISTA+fxml); 
+        cargadorFXML.setBuilderFactory(new JavaFXBuilderFactory());
+        cargadorFXML.setLocation(Principal.class.getResource(PAQUETE_VISTA+fxml));  
+        escena = new Scene((AnchorPane)cargadorFXML.load(archivo), ancho, alto);
+        escenarioPrincipal.setScene(escena);
+        escenarioPrincipal.sizeToScene();
+        resultado = (Initializable)cargadorFXML.getController();
+        return resultado; 
+            
+    }  
+   
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+        
+    }
+    
 }
