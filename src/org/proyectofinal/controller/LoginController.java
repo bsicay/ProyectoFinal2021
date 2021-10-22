35066f0074d1e693494e4616ead1bcf0a1c64f1c
Brandon <sicayb@gmail,com>
2021-10-21 19:56:01 -0600

    Agregando ventana inicial de login

---
+++ src/org/proyectofinal/controller/LoginController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.proyectofinal.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import org.proyectofinal.sistema.Principal;
+
+/**
+ *
+ * @author brand
+ */
+public class LoginController implements Initializable{
+    
+    private Principal escenarioPrincipal;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+            // TODO Auto-generated method stub
+
+    }
+
+    public Principal getEscenarioPrincipal() {
+
+    return escenarioPrincipal;
+    }
+
+    public void setEscenarioPrincipal(Principal escenarioPrincipal) {
+        this.escenarioPrincipal = escenarioPrincipal;
+    }
+
+    
+}
