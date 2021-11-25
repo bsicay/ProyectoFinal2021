cfbd42c0fa98e378285cddb8ebf85b4a42d52ac9
Brandon <sicayb@gmail,com>
2021-11-25 09:13:08 -0600

    Agregando dificutad

--- src/org/proyectofinal/controller/AgregarCursosController.java
+++ src/org/proyectofinal/controller/AgregarCursosController.java
@@ -7,7 +7,14 @@ package org.proyectofinal.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import org.proyectofinal.sistema.Principal;
 
 /**
@@ -16,11 +23,28 @@ import org.proyectofinal.sistema.Principal;
  */
 public class AgregarCursosController implements Initializable{
     private Principal escenarioPrincipal;
+    @FXML
+    private TextField txtNombreCurso;
+    @FXML
+    private TextArea txtDescripcionCurso;
+    @FXML
+    private TextField txtDuracion;
+    @FXML
+    private CheckBox chkCursoEspecial;
+    @FXML
+    private ComboBox cmbDificultad;
+    @FXML
+    private Button btnAgregar;
+  
+    
 	
     @Override
     public void initialize(URL location, ResourceBundle resources) {
             // TODO Auto-generated method stub
-
+            cmbDificultad.getItems().clear();
+            cmbDificultad.getItems().addAll("Principiante",
+            "Intermedio",
+            "Avanzado");
     }
         
         
--- src/org/proyectofinal/controller/MenuProfesorController.java
+++ src/org/proyectofinal/controller/MenuProfesorController.java
@@ -7,7 +7,10 @@ package org.proyectofinal.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import org.proyectofinal.sistema.Principal;
 
 /**
@@ -17,11 +20,52 @@ import org.proyectofinal.sistema.Principal;
 public class MenuProfesorController implements Initializable{
     
     private Principal escenarioPrincipal;
+    @FXML private ImageView curso;
+    @FXML private ImageView libros;
+    @FXML private ImageView misCursos;
+    @FXML private ImageView miPerfil;
 	
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         
+    }
+    
+    
+    public void vistaCurso(){
+       escenarioPrincipal.agregarCursos();
+    }
+    
+    public void zoom(){
+        curso.setImage(new Image("/org/proyectofinal/images/menuPrincipal/cursosZoom.png"));
+    }
+    
+    public void normal(){
+        curso.setImage(new Image("/org/proyectofinal/images/menuPrincipal/cursos.png"));
+    }
+    
+    public void librosZoom(){
+        libros.setImage(new Image("/org/proyectofinal/images/menuPrincipal/librosZoom.png"));
+    }
+    
+    public void librosNormal(){
+        libros.setImage(new Image("/org/proyectofinal/images/menuPrincipal/libros.png"));
+    }
+    
+    public void misCursosNormal(){
+        misCursos.setImage(new Image("/org/proyectofinal/images/menuPrincipal/misCursos.png"));
+    }
+    
+    public void misCursosZoom(){
+        misCursos.setImage(new Image("/org/proyectofinal/images/menuPrincipal/misCursosZoom.png"));
+    }
+    
+    public void perfilNormal(){
+        miPerfil.setImage(new Image("/org/proyectofinal/images/menuPrincipal/perfil.png"));
+    }
+    
+    public void perfilZoom(){
+        miPerfil.setImage(new Image("/org/proyectofinal/images/menuPrincipal/perfilZoom.png"));
     }
     
     public void video(){
